a7d904e9cdd298de71fa9227ef36c1245a292c0f
Lupita <lupita.dav@gmail.com>
2017-05-17 17:24:30 -0700

    Working nextImage function with carousel

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,25 @@
-var gallery = document.getElementsByClassName("carousel");
-var images = document.getElementsByClassName("carousel-item")
-var active = document.getElementsByClassName("active");
+// remove active class from current image and set to next
+function nextImage(){
+  var images = document.getElementsByClassName("carousel")[0].getElementsByClassName('carousel-item');
+  console.log(images);
+  var nextActive = 1;
+  for(var i = 0; i < images.length; i++){
+    if(classExists(images[i],"active")){
+      images[i].classList.toggle("active");
+      if(i === images.length-1){
+        nextActive = 0;
+      }else{
+        nextActive = i+1;
+      }
+    }
+  }
+  images[nextActive].classList.toggle("active");
+}
 
-console.log(images);
+setInterval(nextImage, 3000);
+
+
+// Function checks if class exists in element
+function classExists(elem, classname){
+  return (' ' + elem.className + ' ').indexOf(' ' + classname + ' ') > -1;
+}
